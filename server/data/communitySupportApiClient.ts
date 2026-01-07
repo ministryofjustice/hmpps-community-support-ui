7c07105d6fad8eae23da47ef52cad64ce7977aab
Francis O'Brien <francis.obrien@justice.gov.uk>
2026-01-07 11:51:47 +0000

    Update API

--- server/data/communitySupportApiClient.ts
+++ server/data/communitySupportApiClient.ts
@@ -1,6 +1,6 @@
 import { ApiConfig, RestClient, asSystem } from '@ministryofjustice/hmpps-rest-client'
 import type { AuthenticationClient } from '@ministryofjustice/hmpps-auth-clients'
-import type { Referral } from '@community-support-api'
+import type { CommunitySupportServicesProvider, Referral, Person } from '@community-support-api'
 import config from '../config'
 import logger from '../../logger'
 
@@ -11,5 +11,16 @@ export default class CommunitySupportApiClient extends RestClient {
 
   async getReferralById(referralId: string, username: string): Promise<Referral> {
     return this.get({ path: `/referrals/${referralId}` }, asSystem(username))
+  }
+
+  async getCommunitySupportServiceProviders(
+    personDetailsId: string,
+    username: string,
+  ): Promise<CommunitySupportServicesProvider> {
+    return this.get({ path: `/bff/referral-select-a-service?personDetailsId=${personDetailsId}` }, asSystem(username))
+  }
+
+  async getPersonByIdentifier(personIdentifier: string, username: string): Promise<Person> {
+    return this.get({ path: `/person/${personIdentifier}` }, asSystem(username))
   }
 }
