ed99ebeeca682873fd5a5dc220c9f09f90ac010d
Hannah Ellis <Hannah.Ellis2@justice.gov.uk>
2026-02-27 11:33:24 +0000

    adding acceptance criteria one test

--- integration_tests/pages/AssignPage.ts
+++ integration_tests/pages/AssignPage.ts
@@ -4,13 +4,17 @@ import AbstractPage from './abstractPage'
 export default class AssignPage extends AbstractPage {
   public readonly header: Locator
 
+  public readonly subheader: Locator
+
   private constructor(page: Page) {
     super(page)
     this.header = page.locator('h1')
+    this.subheader = page.locator('legend')
   }
 
   static async verifyOnPage(page: Page): Promise<AssignPage> {
     const assignPage = new AssignPage(page)
+    await expect(assignPage.header).toBeVisible()
     return assignPage
   }
 }
--- integration_tests/specs/assign.spec.ts
+++ integration_tests/specs/assign.spec.ts
@@ -11,11 +11,21 @@ test.describe('AssignPage', () => {
     await communitySupport.stubGetReferralUserAssignments()
     await page.goto('/')
     await login(page)
+    await test.step('go to assign page', async () => {
+      await page.goto(`/referral/${id}/assign`)
+    })
   })
 
   test('should display the page', async ({ page }) => {
-    await page.goto(`/referral/${id}/assign`)
     const assignPage = await AssignPage.verifyOnPage(page)
     expect(assignPage.header).toBeVisible()
+  })
+  // IPB-2010:AC1
+  test('Assigning cases headers', async ({ page }) => {
+    const assignPage = await AssignPage.verifyOnPage(page)
+    await test.step('check content', async () => {
+      await expect(assignPage.header).toHaveText('Who do you want to assign this case to?')
+      await expect(assignPage.subheader).toHaveText('Caseworker')
+    })
   })
 })
