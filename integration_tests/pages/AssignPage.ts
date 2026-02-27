2a41d6e31673cea713d87155d75a61e07fff1fdf
moj-keith-au <Keith.Au@justice.gov.uk>
2026-02-27 14:53:52 +0000

    AC3

--- integration_tests/pages/AssignPage.ts
+++ integration_tests/pages/AssignPage.ts
@@ -6,15 +6,31 @@ export default class AssignPage extends AbstractPage {
 
   public readonly subheader: Locator
 
+  public emailAddressInputs: Locator[]
+
+  public readonly addAnotherCaseWorkerButton: Locator
+
   private constructor(page: Page) {
     super(page)
     this.header = page.locator('h1')
     this.subheader = page.locator('legend')
+    this.addAnotherCaseWorkerButton = page.locator('[data-testid="addAnotherCaseWorker"]')
+    this.emailAddressInputs = []
+  }
+
+  async updateInputs() {
+    const inputs = this.page.locator('[data-testid="caseWorkerInput"]')
+    this.emailAddressInputs = await inputs.count().then(count =>
+      Array(count)
+        .fill(0)
+        .map((_, i) => inputs.nth(i)),
+    )
   }
 
   static async verifyOnPage(page: Page): Promise<AssignPage> {
     const assignPage = new AssignPage(page)
     await expect(assignPage.header).toBeVisible()
+    await assignPage.updateInputs()
     return assignPage
   }
 }
--- integration_tests/specs/assign.spec.ts
+++ integration_tests/specs/assign.spec.ts
@@ -4,6 +4,11 @@ import { login, resetStubs } from '../testUtils'
 import communitySupport from '../mockApis/communitySupport'
 import AssignPage from '../pages/AssignPage'
 
+/*
+  Test log
+  - what happen if we fill the first input in the second again and then do the assignment
+*/
+
 test.describe('AssignPage', () => {
   const id = randomUUID()
   test.beforeEach(async ({ page }) => {
@@ -26,6 +31,71 @@ test.describe('AssignPage', () => {
     await test.step('check content', async () => {
       await expect(assignPage.header).toHaveText('Who do you want to assign this case to?')
       await expect(assignPage.subheader).toHaveText('Caseworker')
+    })
+  })
+  // IPB-2010:AC2 - !!! No back link yet!!!
+  test.skip('Back navigation', async () => {
+    /*
+      Given I’m assigning a specific case to caseworker(s)
+      When I select the Back option
+      Then I am taken back to the specific case’s referral details screen
+      */
+  })
+  // IPB-2010:AC3
+  test('Adding Multiple Caseworkers', async ({ page }) => {
+    const assignPage = await AssignPage.verifyOnPage(page)
+    await test.step('assign case worker 1', async () => {
+      await expect(assignPage.emailAddressInputs).toHaveLength(1)
+      await assignPage.emailAddressInputs[0].fill('test1@email.com')
+      await assignPage.addAnotherCaseWorkerButton.click()
+      await assignPage.updateInputs()
+      await expect(assignPage.emailAddressInputs).toHaveLength(2)
+    })
+    await test.step('assign case worker 2', async () => {
+      await expect(assignPage.emailAddressInputs).toHaveLength(2)
+      await assignPage.emailAddressInputs[1].fill('test2@email.com')
+      await assignPage.addAnotherCaseWorkerButton.click()
+      await assignPage.updateInputs()
+      await expect(assignPage.emailAddressInputs).toHaveLength(3)
+    })
+    await test.step('assign case worker 3', async () => {
+      await expect(assignPage.emailAddressInputs).toHaveLength(3)
+      await assignPage.emailAddressInputs[2].fill('test3@email.com')
+      await assignPage.addAnotherCaseWorkerButton.click()
+      await assignPage.updateInputs()
+      await expect(assignPage.emailAddressInputs).toHaveLength(4)
+    })
+    await test.step('assign case worker 4', async () => {
+      await expect(assignPage.emailAddressInputs).toHaveLength(4)
+      await assignPage.emailAddressInputs[3].fill('test4@email.com')
+      await assignPage.addAnotherCaseWorkerButton.click()
+      await assignPage.updateInputs()
+      await expect(assignPage.emailAddressInputs).toHaveLength(5)
+    })
+    await test.step('assign the last case worker', async () => {
+      await expect(assignPage.emailAddressInputs).toHaveLength(5)
+      await assignPage.emailAddressInputs[4].fill('test5@email.com')
+      await expect(assignPage.addAnotherCaseWorkerButton).toContainClass('govuk-visually-hidden')
+      await assignPage.updateInputs()
+      await expect(assignPage.emailAddressInputs).toHaveLength(5)
+    })
+  })
+  // IPB-2010: what happen if we fill the first input in the second again and then do the assignment
+  test('Adding two case workers in the same input', async ({ page }) => {
+    const assignPage = await AssignPage.verifyOnPage(page)
+    await test.step('assign a case worker', async () => {
+      await expect(assignPage.emailAddressInputs).toHaveLength(1)
+      await assignPage.emailAddressInputs[0].fill('test1@email.com')
+      await assignPage.addAnotherCaseWorkerButton.click()
+      await assignPage.updateInputs()
+      await expect(assignPage.emailAddressInputs).toHaveLength(2)
+    })
+    await test.step('assign another case worker', async () => {
+      await expect(assignPage.emailAddressInputs).toHaveLength(2)
+      await assignPage.emailAddressInputs[0].fill('test2@email.com')
+      await assignPage.addAnotherCaseWorkerButton.click()
+      await assignPage.updateInputs()
+      await expect(assignPage.emailAddressInputs).toHaveLength(3)
     })
   })
 })
