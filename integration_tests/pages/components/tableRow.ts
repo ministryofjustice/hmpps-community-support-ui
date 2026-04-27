cd4c0944ae807323952f0a124a7a06a55d38c6b5
Hannah Ellis <Hannah.Ellis2@justice.gov.uk>
2026-04-27 11:15:11 +0100

    woo no more weird code using nth

--- integration_tests/pages/components/tableRow.ts
+++ integration_tests/pages/components/tableRow.ts
@@ -4,15 +4,7 @@ export default class TableRow {
   static create(rowLocator: Locator, rowType: 'td' | 'th'): Promise<TableRow> {
     const elementsLocator = rowLocator.locator(rowType)
 
-    return elementsLocator
-      .count()
-      .then(elementsCount =>
-        Array(elementsCount)
-          .fill(0)
-          .map((_, i) => elementsLocator.nth(i)),
-      )
-      .then(rowPromise => Promise.all(rowPromise))
-      .then(locators => new TableRow(rowLocator, locators))
+    return elementsLocator.all().then(locators => new TableRow(rowLocator, locators))
   }
 
   private constructor(
