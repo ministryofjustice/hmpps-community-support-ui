78fcbc035d16f47e715a8a28a4ddaf36cf5cb0ef
Hannah Ellis <Hannah.Ellis2@justice.gov.uk>
2026-05-21 08:24:50 +0100

    adding create factory function to text area for consistancy

--- integration_tests/pages/components/textArea.ts
+++ integration_tests/pages/components/textArea.ts
@@ -1,18 +1,18 @@
 import { Locator } from '@playwright/test'
 
 export default class TextArea {
-  public readonly input: Locator
-
-  public readonly label: Locator
-
-  public readonly hint: Locator
-
-  public readonly errorText: Locator
-
-  constructor(textAreaLocator: Locator) {
-    this.input = textAreaLocator.locator('> textarea.govuk-textarea')
-    this.label = textAreaLocator.locator('> h1.govuk-label-wrapper')
-    this.hint = textAreaLocator.locator('> .govuk-hint')
-    this.errorText = textAreaLocator.locator('> p.govuk-error-message')
+  static create(textAreaLocator: Locator): Promise<TextArea> {
+    const input = textAreaLocator.locator('> textarea.govuk-textarea')
+    const label = textAreaLocator.locator('> h1.govuk-label-wrapper')
+    const hint = textAreaLocator.locator('> .govuk-hint')
+    const errorText = textAreaLocator.locator('> p.govuk-error-message')
+    return Promise.resolve(new TextArea(input, label, hint, errorText))
   }
+
+  private constructor(
+    public readonly input: Locator,
+    public readonly label: Locator,
+    public readonly hint: Locator,
+    public readonly errorText: Locator,
+  ) {}
 }
