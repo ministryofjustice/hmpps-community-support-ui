fcbe9fa2a5a091e0c4a23dbe1b5ed66e9164b294
Davansah <david.ansah@digital.justice.gov.uk>
2026-06-24 15:02:21 +0100

    IPB-2389: Reverted strong typing on RedisClient

--- server/data/redisClient.ts
+++ server/data/redisClient.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'redis'
+import { createClient, RedisClientType } from 'redis'
 
 import logger from '../../logger'
 import config from '../config'
@@ -8,7 +8,7 @@ const url =
     ? `rediss://${config.redis.host}:${config.redis.port}`
     : `redis://${config.redis.host}:${config.redis.port}`
 
-const createRedisClient = () => {
+const createRedisClient = (): RedisClientType => {
   const client = createClient({
     url,
     password: config.redis.password,
