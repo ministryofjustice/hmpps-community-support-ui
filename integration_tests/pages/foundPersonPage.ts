eeb24abe3d68704299b373d3d45fa19bd4cd874f
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2026-07-07 15:35:12 +0100

    fixing the issues found in confirm person page

--- integration_tests/pages/foundPersonPage.ts
+++ integration_tests/pages/foundPersonPage.ts
@@ -5,19 +5,30 @@ import SummaryList from './components/summaryList'
 export default class FoundPersonPage extends AbstractPage {
   readonly header: Locator
 
+  readonly backLink: Locator
+
   readonly personSummary: SummaryList
+
+  readonly enterDifferentIdentifierLink: Locator
 
   private constructor(page: Page, personSummary: SummaryList) {
     super(page)
     this.header = page.locator('h1', { hasText: 'Confirm this is the correct person for referral' })
+    this.backLink = page.getByRole('link', { name: 'Back', exact: true })
     this.personSummary = personSummary
+    this.enterDifferentIdentifierLink = page.getByRole('link', {
+      name: 'Enter a different CRN or prison number',
+      exact: true,
+    })
   }
 
   static async verifyOnPage(page: Page): Promise<FoundPersonPage> {
     const personSummary = await SummaryList.create(page.locator('[data-testid="personsummary"]'))
     const foundPersonPage = new FoundPersonPage(page, personSummary)
     await expect(foundPersonPage.header).toBeVisible()
+    await expect(foundPersonPage.backLink).toBeVisible()
     await expect(foundPersonPage.personSummary.summaryLocator).toBeVisible()
+    await expect(foundPersonPage.enterDifferentIdentifierLink).toBeVisible()
     return foundPersonPage
   }
 }
--- server/testutils/factories/FoundPersonContent.ts
+++ server/testutils/factories/FoundPersonContent.ts
@@ -7,4 +7,8 @@ export default FoundPersonContentFactory.define(({ transientParams }) => ({
   pageHeader: transientParams.pageHeader || `Confirm this is the correct person for referral`,
   continueButtonText: transientParams.continueButtonText || 'Continue',
   continueButtonLink: transientParams.continueButtonLink || '/referral/new/select-a-service',
+  backLink: transientParams.backLink || '/referral/new/find-a-person',
+  enterDifferentIdentifierLinkText:
+    transientParams.enterDifferentIdentifierLinkText || 'Enter a different CRN or prison number',
+  enterDifferentIdentifierLinkHref: transientParams.enterDifferentIdentifierLinkHref || '/referral/new/find-a-person',
 }))
