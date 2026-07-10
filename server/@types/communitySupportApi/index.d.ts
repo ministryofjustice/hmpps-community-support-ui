eec24a99a693e148bcc0ab8c5ce75bd8e6b0a615
Davansah <david.ansah@digital.justice.gov.uk>
2026-07-10 09:45:40 +0100

    IPB-2400: Scheduling ICS Appointments Retrospectively (#224)

--- server/@types/communitySupportApi/index.d.ts
+++ server/@types/communitySupportApi/index.d.ts
@@ -19,6 +19,7 @@ type ReferralUserAssignmentsResponse = components['schemas']['ReferralUserAssign
 type ReferralUserAssignmentsDto = components['schemas']['ReferralUserAssignmentsDto']
 type ReferralDetailsResponseDto = components['schemas']['ReferralDetailsBffResponseDto']
 type AppointmentIcsResponse = components['schemas']['AppointmentIcsResponse']
+type AppointmentTimeResponse = components['schemas']['AppointmentIcsResponse']['appointmentTime']
 type ReferralProgress = components['schemas']['ReferralProgressDto']
 type ReferralAppointmentHistory = components['schemas']['ReferralAppointmentHistoryDto']
 type ProbationOffice = components['schemas']['ProbationOffice']
@@ -50,6 +51,7 @@ export type {
   CaseWorkerDto,
   ReferralDetailsResponseDto,
   AppointmentIcsResponse,
+  AppointmentTimeResponse,
   ReferralProgress,
   ReferralAppointmentHistory,
   ProbationOffice,
