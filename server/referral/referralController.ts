8678391bb691e0de571eca3be7be045842d39272
hannah-ellis2 <hannah.ellis2@justice.gov.uk>
2026-07-14 11:08:23 +0100

    fixing a typo (#238)

--- server/referral/referralController.ts
+++ server/referral/referralController.ts
@@ -18,7 +18,7 @@ import {
   updateSectionStatus,
 } from './taskList/TaskListHelper'
 import ConfirmPersonalDetailsPresenter from './confirmPersonalDetails/ConfirmPersonalDetailsPresenter'
-import AdditionalSuportNeedsPresenter from './additionalSupporNeeds/AdditionalSupportNeedsPresenter'
+import AdditionalSuportNeedsPresenter from './additionalSupportNeeds/AdditionalSupportNeedsPresenter'
 
 export default class ReferralController {
   private static readonly CRN_REGEX = /^[A-Za-z]\d{6}$/
