ce9d731e2693d36e3e2e85d7054d8e16f38ca2a7
Hannah Ellis <Hannah.Ellis2@justice.gov.uk>
2026-07-21 16:16:11 +0100

    remove some commented out code

--- integration_tests/full/taskList.spec.ts
+++ integration_tests/full/taskList.spec.ts
@@ -49,7 +49,6 @@ test.describe('Task List Page', () => {
   })
 
   test('should display task list correctly after updated task status', async ({ page }) => {
-    // await seedSessionTaskListState(page, mockPersonId, 'riskInformation', 'completed')
     await page.goto(TaskListPage.url(mockReferralId))
     const taskListPage = await TaskListPage.verifyOnPage(page)
     await taskListPage.verifyTaskStatus('Personal details', 'Confirm personal details', 'Incomplete')
@@ -70,11 +69,6 @@ test.describe('Task List Page', () => {
   })
 
   test('should display check answers status correctly after updated all task status to completed', async ({ page }) => {
-    // await seedSessionTaskListState(page, mockPersonId, 'personalDetails', 'completed')
-    // await seedSessionTaskListState(page, mockPersonId, 'riskInformation', 'completed')
-    // await seedSessionTaskListState(page, mockPersonId, 'personNeeds', 'completed')
-    // await seedSessionTaskListState(page, mockPersonId, 'supportNeeds', 'completed')
-    // await seedSessionTaskListState(page, mockPersonId, 'contactDetails', 'completed')
     await page.goto(TaskListPage.url(mockReferralId))
     const taskListPage = await TaskListPage.verifyOnPage(page)
     await taskListPage.verifyTaskStatus('Personal details', 'Confirm personal details', 'Completed')
