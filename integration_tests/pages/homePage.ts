78bead02c700ca8f0e101d87a6bd22fb8d598d25
Francis O'Brien <francis.obrien@justice.gov.uk>
2026-07-21 13:18:41 +0100

    Update landing page tiles

--- integration_tests/pages/homePage.ts
+++ integration_tests/pages/homePage.ts
@@ -4,14 +4,35 @@ import AbstractPage from './abstractPage'
 export default class HomePage extends AbstractPage {
   readonly header: Locator
 
+  readonly makeAReferralTile: Locator
+
+  readonly viewCasesTile: Locator
+
+  readonly tileLinks: Locator
+
   private constructor(page: Page) {
     super(page)
-    this.header = page.locator('h1', { hasText: 'Welcome to the Community Support service.' })
+    this.header = page.getByRole('heading', { level: 1, name: 'Community Support' })
+    this.makeAReferralTile = page.getByRole('link', { name: 'Make a referral' })
+    this.viewCasesTile = page.getByRole('link', { name: 'View cases' })
+    this.tileLinks = page.locator('.card__link')
   }
 
   static async verifyOnPage(page: Page): Promise<HomePage> {
     const homePage = new HomePage(page)
     await expect(homePage.header).toBeVisible()
     return homePage
+  }
+
+  async expectTileOrder(expectedOrder: string[]): Promise<void> {
+    await expect(this.tileLinks).toHaveText(expectedOrder)
+  }
+
+  async clickMakeAReferralTile(): Promise<void> {
+    await this.makeAReferralTile.click()
+  }
+
+  async clickViewCasesTile(): Promise<void> {
+    await this.viewCasesTile.click()
   }
 }
