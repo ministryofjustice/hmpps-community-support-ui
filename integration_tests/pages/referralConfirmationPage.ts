3a7a6947cf4acb7e35fafa64567788d26c6b1cef
moj-keith-au <Keith.Au@justice.gov.uk>
2026-08-13 15:28:18 +0100

    - change "start a new referral" link to a button - add back to home page link - add and update test cases

--- integration_tests/pages/referralConfirmationPage.ts
+++ integration_tests/pages/referralConfirmationPage.ts
@@ -8,11 +8,17 @@ export default class ReferralConfirmationPage extends AbstractPage {
 
   readonly referenceNumber: Locator
 
+  readonly startNewReferralButton: Locator
+
+  readonly backToCommunityHomeLink: Locator
+
   private constructor(page: Page) {
     super(page)
     this.header = page.locator('h1', { hasText: 'The referral has been sent' })
     this.panelMessage = page.locator('text=Your reference number')
     this.referenceNumber = page.locator('strong', { hasText: 'QD0878DE' })
+    this.startNewReferralButton = page.getByRole('button', { name: 'Start a new referral' })
+    this.backToCommunityHomeLink = page.locator('a', { hasText: 'Back to Community Support Homepage' })
   }
 
   static async verifyOnPage(page: Page): Promise<ReferralConfirmationPage> {
@@ -20,6 +26,10 @@ export default class ReferralConfirmationPage extends AbstractPage {
     await expect(referralConfirmationPage.header).toBeVisible()
     await expect(referralConfirmationPage.panelMessage).toBeVisible()
     await expect(referralConfirmationPage.referenceNumber).toBeVisible()
+    await expect(referralConfirmationPage.startNewReferralButton).toBeVisible()
+    await expect(referralConfirmationPage.startNewReferralButton).toHaveAttribute('href', '/referral/new/find-a-person')
+    await expect(referralConfirmationPage.backToCommunityHomeLink).toBeVisible()
+    await expect(referralConfirmationPage.backToCommunityHomeLink).toHaveAttribute('href', '/')
     return referralConfirmationPage
   }
 }
