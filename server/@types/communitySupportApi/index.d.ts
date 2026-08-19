6c35d87750888ee4e0e069b7472d64342af378d1
DanEgan1 <daniel.egan1@justice.gov.uk>
2026-08-19 14:32:16 +0100

    IPB-2603 setup dto in ui

--- server/@types/communitySupportApi/index.d.ts
+++ server/@types/communitySupportApi/index.d.ts
@@ -86,6 +86,7 @@ export type {
   ConfirmPersonDetailsBffDto,
   AdditionalSupportNeedsDto,
   ServiceEndDatePageDto,
+  ServiceDaysPageDto,
   NeedsInterpreterBffResponseDto,
   Selection,
   CommunitySupportRiskDto,
