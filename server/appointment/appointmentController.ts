4fba99b8c7dfa3c8d622f7e017dbc4ffe88d0363
DanEgan1 <daniel.egan1@justice.gov.uk>
2026-09-03 11:26:08 +0100

    review changes

--- server/appointment/appointmentController.ts
+++ server/appointment/appointmentController.ts
@@ -26,10 +26,7 @@ import { ReferralProgressBannerContent } from '../referral/progress/ReferralProg
 import AppointmentValidator from './AppointmentValidator'
 import { IcsFeedbackHowSessionTookPlaceFormData } from './ics-feedback/icsFeedbackHowSessionTookPlaceViewModel'
 import { getChangeRequesterType } from './change-ics-details-reason/ChangeAppointmentDetails'
-import {
-  SessionFeedbackFormContent,
-  SessionFeedbackFormDataSchemaBuilder,
-} from '../validation/SessionFeedbackFormData'
+import { SessionFeedbackFormContent, SessionFeedbackFormDataSchemaBuilder } from '../validation/SessionFeedbackFormData'
 import ViewChangeSessionDetailsPresenter from './view-change-session-details/ViewChangeSessionDetailsPresenter'
 import RecordSessionDetailsPresenter from './record-ics/RecordSessionDetailsPresenter'
 import { RecordSessionDetailsFormDataSchema } from '../validation/RecordSessionDetailsFormData'
