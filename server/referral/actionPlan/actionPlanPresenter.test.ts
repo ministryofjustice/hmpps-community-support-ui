70f44cdbee33f740b6783274e122e929c0340a85
Thomas Swann <thomas.swann@justice.gov.uk>
2026-09-14 12:00:28 +0100

    IPB-2756 PR comments

--- server/referral/actionPlan/actionPlanPresenter.test.ts
+++ server/referral/actionPlan/actionPlanPresenter.test.ts
@@ -26,11 +26,5 @@ describe('ActionPlanPresenter', () => {
 
     expect(renderData.content.pageHeader).toBe('Action plan for Alex River')
     expect(renderData.content.backLink).toEqual({ href: '/progress/AB1234CD' })
-    // expect(renderData.content.needsSummary.rows).toEqual([
-    //   {
-    //     key: { text: 'Needs' },
-    //     value: { html: '<strong class="govuk-tag govuk-tag--blue">In progress</strong>' },
-    //   },
-    // ])
   })
 })
