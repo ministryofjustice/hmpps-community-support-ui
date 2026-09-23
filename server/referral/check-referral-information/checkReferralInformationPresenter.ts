ca59c41500bb2c39464774d3e0fb7046176d34ac
DanEgan1 <daniel.egan1@justice.gov.uk>
2026-09-23 11:35:54 +0100

    merge

--- server/referral/check-referral-information/checkReferralInformationPresenter.ts
+++ server/referral/check-referral-information/checkReferralInformationPresenter.ts
@@ -314,11 +314,7 @@ export default class CheckReferralInformationPresenter extends PresenterBase<
       govFrontendSummaryListRow(cardContent.phoneNumberLabel, data.phoneNumber || notAvailable),
       govFrontendSummaryListRow(cardContent.mobileNumberLabel, data.mobileNumber || notAvailable),
       govFrontendSummaryListRow(cardContent.emailAddressLabel, data.email || notAvailable),
-      govFrontendSummaryListRow({ html: cardContent.mainAddressLabel }, { html: formattedAddress }),
-      govFrontendSummaryListRow(
-        { html: labelWithLastUpdated(addressLabel, lastUpdatedLabel, data.addressUpdatedAt || notAvailable) },
-        { html: formattedAddress },
-      ),
+      govFrontendSummaryListRow({ html: addressLabel }, { html: formattedAddress }),
     ]
 
     return {
